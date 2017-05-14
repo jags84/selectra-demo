5ed1aeb01d4c8d284c40ab3a73f1e001822a9f6b
Juan Gonzalez <jags84@gmail.com>
2017-05-14 11:01:18 -0400

    Location Schedule week days, Devise config

--- app/models/location_schedule.rb
+++ app/models/location_schedule.rb
@@ -1,3 +1,11 @@
 class LocationSchedule < ApplicationRecord
   belongs_to :location
+  validates :day, :open_time, :close_time, :location, presence: true
+  WEEK_DAYS = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+
+  # Return all week days
+  def self.get_week_days
+    WEEK_DAYS
+  end
+
 end
