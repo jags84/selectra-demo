0fd2e29a0025177a078899d9372f9cf3d06c6814
Juan Gonzalez <jags84@gmail.com>
2017-05-14 10:54:19 -0400

    Locations spec

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ApplicationRecord
   has_many :location_schedules
+  validates :name,:address, presence: true
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(name: "My Location Name", address: "My Location Address")
+  }
+
+  describe "Record Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a name" do
+      subject.name = ""
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without an address" do
+      subject.address = ""
+      expect(subject).to_not be_valid
+    end
+  end
 end
