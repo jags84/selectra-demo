21e0dd6debd7086357c8434ca473ec8dfb4afea0
Juan Gonzalez <jags84@gmail.com>
2017-05-14 11:41:39 -0400

    User Edit and User Specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(email: "some@domain.com", password: "123456", password_confirmation: "123456")
+  }
+
+  describe "Record Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without an email" do
+      subject.email = ""
+      expect(subject).to_not be_valid
+    end
+  end
+
 end
